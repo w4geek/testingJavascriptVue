e3b835e58f93a291ee274e878e37a9f58cc07d2e
w4geek <92009661+w4geek@users.noreply.github.com>
2021-11-04 00:47:56 -0300

    my-first-commit

---
+++ js/index.js
@@ -0,0 +1,59 @@
+let a = "Hola Mundo";
+document.write(a);
+
+let b = "Alerta de prueba de javascript";
+console.log(b);
+
+let c = "Otra alerta de prueba de Javascript";
+console.warn(c);
+
+let d = `Otro comentario estúpido`;
+document.write(d);
+
+let e = 'Otro comentario estúpido 2';
+console.log("idiota", 5 + 4, "otra cosa");
+
+var iva = 0.21;
+iva = 0.15;
+console.log(iva);
+
+const PI = 3.1416;
+//PI = 0.15;
+console.log(PI);
+
+let f = true;
+
+let u;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(iva);
+console.log(PI);
+console.log(f);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(iva));
+console.log(typeof(PI));
+console.log(typeof(f));
+console.log(typeof(u));
+
+let objeto_2 = new Object();
+objeto_2.nombre = "Juan";
+objeto_2.apellido = "Perez";
+objeto_2.edad = 30;
+console.log(objeto_2);
+
+
+let objeto = {var1: 23, var2: true, var3: "Hola Mundo"};
+console.log(objeto);
+
+let testprompt = prompt("Ingrese su nombre","");
+document.write("Hola :" + testprompt);
+
